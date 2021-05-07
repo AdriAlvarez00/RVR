04f67127f627362fd7dc07875f8f7aae948ae6ac
AdriAlvarez00 <adralv01@ucm.es>
2021-05-07 20:51:19 +0200

    Ejercicio 1 encaminado
    Problemas con IPv6

---
+++ P1/gai.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char** argv)
+{
+    if(argc != 2){
+        std::cerr << "Expected use gai www.myweb.domain\n";
+    }
+
+    addrinfo hints;
+    addrinfo* result;
+    memset(&hints,0,sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;//IPv4 and IPv6
+    
+    //TODO some ipv6 not resolving
+    //localhost not ::1
+    //147.96.1.9 also not
+    int res = getaddrinfo(argv[1], NULL,&hints,&result);
+
+    if(res!=0){
+        std::cerr << gai_strerror(res) << "\n";
+        return 1;
+    }
+
+    while(result!=nullptr){
+        char host[NI_MAXHOST];
+        char service[NI_MAXSERV];
+
+        getnameinfo(result->ai_addr,result->ai_addrlen,host,NI_MAXHOST,service,NI_MAXSERV,NI_NUMERICHOST|NI_NUMERICSERV);
+
+        std::cout << host << "\t" << result->ai_family << "\t" << result->ai_socktype << "\n";
+        result = result->ai_next;
+    }
+ 
+    freeaddrinfo(result);
+    return 0;
+}
